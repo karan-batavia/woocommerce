794a75a22e4054eecda714c0daa5d80f124927d9
“Joshua <joshuatf@gmail.com>
2025-02-12 15:58:16 -0500

    Update cache service to use new aggregate methods

--- plugins/woocommerce/src/Caches/OrderAggregateCacheService.php
+++ plugins/woocommerce/src/Caches/OrderAggregateCacheService.php
@@ -26,7 +26,6 @@ class OrderAggregateCacheService {
 		$this->order_aggregate_cache = $order_aggregate_cache;
 		add_action( 'woocommerce_new_order', array( $this, 'update_on_new_order' ) );
 		add_action( 'woocommerce_order_status_changed', array( $this, 'update_on_order_status_changed' ), 10, 3 );
-		add_action( 'shutdown', array( $this, 'persist_cache' ) );
 	}
 
 	/**
@@ -44,18 +43,7 @@ class OrderAggregateCacheService {
 	 * @param string $next_status the new WooCommerce order status.
 	 */
 	public function update_on_order_status_changed( $order_id, $previous_status, $next_status ) {
-		$previous_status_count = $this->order_aggregate_cache->get_count( 'wc-' . $previous_status ) - 1;
-		$next_status_count     = $this->order_aggregate_cache->get_count( 'wc-' . $next_status ) + 1;
-
-		$this->order_aggregate_cache->update_count( 'wc-' . $previous_status, $previous_status_count );
-		$this->order_aggregate_cache->update_count( 'wc-' . $next_status, $next_status_count );
+		$this->order_aggregate_cache->decrement_count_for_status( 'wc-' . $previous_status );
+		$this->order_aggregate_cache->increment_count_for_status( 'wc-' . $next_status );
 	}
-
-	/**
-	 * Persist any changes to the cache on shutdown.
-	 */
-	public function persist_cache() {
-		$this->order_aggregate_cache->persist_updates();
-	}
-
 }
