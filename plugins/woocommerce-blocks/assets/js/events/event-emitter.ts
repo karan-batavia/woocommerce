81669500130ba9d473f9ab915e1d96bf2e81ddd2
Thomas Roberts <thomas.roberts@automattic.com>
2025-02-13 23:37:42 +0000

    Add createEmitter function to js/events

---
+++ plugins/woocommerce-blocks/assets/js/events/event-emitter.ts
@@ -0,0 +1,65 @@
+/**
+ * External dependencies
+ */
+import {
+	isErrorResponse,
+	isFailResponse,
+	isObserverResponse,
+	type ObserverResponse,
+} from '@woocommerce/types';
+
+export interface EventEmitter {
+	emit: (
+		eventName: string,
+		data?: unknown
+	) => Promise< ObserverResponse[] >;
+	subscribe: ( listener: EventListener, eventName: string ) => VoidFunction;
+}
+
+export type EventListener = (
+	data: unknown
+) => Promise< ObserverResponse | true > | ObserverResponse | true;
+
+/**
+ * Create an event emitter.
+ *
+ * @return The event emitter.
+ */
+export function createEmitter(): EventEmitter {
+	const listeners = new Map< string, Set< EventListener > >();
+	const notifyListeners = async ( eventName: string, data: unknown ) => {
+		const listenersForEvent =
+			listeners.get( eventName ) || new Set< EventListener >();
+		// We use Array.from to clone the listeners Set. This ensures that we don't run a listener that was added as a
+		// response to another listener.
+		const clonedListeners = Array.from( listenersForEvent );
+		const responses = [];
+		for ( const listener of clonedListeners ) {
+			const observerResponse = await listener( data );
+			if ( isObserverResponse( observerResponse ) ) {
+				responses.push( observerResponse );
+			}
+		}
+		return responses;
+	};
+
+	return {
+		subscribe( listener, eventName: string ) {
+			let listenersForEvent =
+				listeners.get( eventName ) || new Set< EventListener >();
+			listenersForEvent.add( listener );
+			listeners.set( eventName, listenersForEvent );
+			return () => {
+				// Re-get the listeners for the event in case the list was updated before unsubscribe was called.
+				listenersForEvent =
+					listeners.get( eventName ) || new Set< EventListener >();
+				listenersForEvent.delete( listener );
+				listeners.set( eventName, listenersForEvent );
+			};
+		},
+
+		emit: async ( eventName: string, data: unknown ) => {
+			return await notifyListeners( eventName, data );
+		},
+	};
+}
