013153b6cb1cfa1abbe8246ff9dafe152e43b6b6
Thomas Roberts <thomas.roberts@automattic.com>
2025-02-13 23:38:09 +0000

    Add checkout-events file that creates the emitter for checkout events

---
+++ plugins/woocommerce-blocks/assets/js/events/checkout-events.ts
@@ -0,0 +1,21 @@
+/**
+ * Internal dependencies
+ */
+import { createEmitter, type EventListener } from './event-emitter';
+
+export const CHECKOUT_EVENTS = {
+	CHECKOUT_SUCCESS: 'checkout_success',
+	CHECKOUT_FAIL: 'checkout_fail',
+	CHECKOUT_VALIDATION: 'checkout_validation',
+};
+
+export const checkoutEventsEmitter = createEmitter();
+const callbackWrapper = ( eventName: string ) => ( callback: EventListener ) =>
+	checkoutEventsEmitter.subscribe( callback, eventName );
+export const checkoutEvents = {
+	onCheckoutValidation: callbackWrapper(
+		CHECKOUT_EVENTS.CHECKOUT_VALIDATION
+	),
+	onCheckoutSuccess: callbackWrapper( CHECKOUT_EVENTS.CHECKOUT_SUCCESS ),
+	onCheckoutFail: callbackWrapper( CHECKOUT_EVENTS.CHECKOUT_FAIL ),
+};
