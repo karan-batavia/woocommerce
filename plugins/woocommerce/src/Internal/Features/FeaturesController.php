390c27f04628241ed247d64d8398513f25cdebd5
Daniel Mallory <daniel.mallory@automattic.com>
2025-02-20 16:03:57 +0000

    Linting

--- plugins/woocommerce/src/Internal/Features/FeaturesController.php
+++ plugins/woocommerce/src/Internal/Features/FeaturesController.php
@@ -183,7 +183,7 @@ class FeaturesController {
 			$tracking_enabled                 = WC_Site_Tracking::is_tracking_enabled();
 
 			$legacy_features = array(
-				'analytics'              => array(
+				'analytics'                          => array(
 					'name'               => __( 'Analytics', 'woocommerce' ),
 					'description'        => __( 'Enable WooCommerce Analytics', 'woocommerce' ),
 					'option_key'         => Analytics::TOGGLE_OPTION_NAME,
@@ -192,7 +192,7 @@ class FeaturesController {
 					'disable_ui'         => false,
 					'is_legacy'          => true,
 				),
-				'product_block_editor'   => array(
+				'product_block_editor'               => array(
 					'name'            => __( 'New product editor', 'woocommerce' ),
 					'description'     => __( 'Try the new product editor (Beta)', 'woocommerce' ),
 					'is_experimental' => true,
@@ -213,13 +213,13 @@ class FeaturesController {
 						return $string;
 					},
 				),
-				'cart_checkout_blocks'   => array(
+				'cart_checkout_blocks'               => array(
 					'name'            => __( 'Cart & Checkout Blocks', 'woocommerce' ),
 					'description'     => __( 'Optimize for faster checkout', 'woocommerce' ),
 					'is_experimental' => false,
 					'disable_ui'      => true,
 				),
-				'rate_limit_checkout'    => array(
+				'rate_limit_checkout'                => array(
 					'name'               => __( 'Rate limit Checkout', 'woocommerce' ),
 					'description'        => sprintf(
 						// translators: %s is the URL to the rate limiting documentation.
@@ -231,7 +231,7 @@ class FeaturesController {
 					'enabled_by_default' => false,
 					'is_legacy'          => true,
 				),
-				'marketplace'            => array(
+				'marketplace'                        => array(
 					'name'               => __( 'Marketplace', 'woocommerce' ),
 					'description'        => __(
 						'New, faster way to find extensions and themes for your WooCommerce store',
@@ -244,7 +244,7 @@ class FeaturesController {
 				),
 				// Marked as a legacy feature to avoid compatibility checks, which aren't really relevant to this feature.
 				// https://github.com/woocommerce/woocommerce/pull/39701#discussion_r1376976959.
-				'order_attribution'      => array(
+				'order_attribution'                  => array(
 					'name'               => __( 'Order Attribution', 'woocommerce' ),
 					'description'        => __(
 						'Enable this feature to track and credit channels and campaigns that contribute to orders on your site',
@@ -255,7 +255,7 @@ class FeaturesController {
 					'is_legacy'          => true,
 					'is_experimental'    => false,
 				),
-				'site_visibility_badge'  => array(
+				'site_visibility_badge'              => array(
 					'name'               => __( 'Site visibility badge', 'woocommerce' ),
 					'description'        => __(
 						'Enable the site visibility badge in the WordPress admin bar',
@@ -267,7 +267,7 @@ class FeaturesController {
 					'is_experimental'    => false,
 					'disabled'           => false,
 				),
-				'hpos_fts_indexes'       => array(
+				'hpos_fts_indexes'                   => array(
 					'name'               => __( 'HPOS Full text search indexes', 'woocommerce' ),
 					'description'        => __(
 						'Create and use full text search indexes for orders. This feature only works with high-performance order storage.',
@@ -278,7 +278,7 @@ class FeaturesController {
 					'is_legacy'          => true,
 					'option_key'         => CustomOrdersTableController::HPOS_FTS_INDEX_OPTION,
 				),
-				'hpos_datastore_caching' => array(
+				'hpos_datastore_caching'             => array(
 					'name'               => __( 'HPOS Data Caching', 'woocommerce' ),
 					'description'        => __(
 						'Enable order data caching in the datastore. This feature only works with high-performance order storage.',
@@ -304,7 +304,7 @@ class FeaturesController {
 					),
 					'option_key'         => CustomOrdersTableController::HPOS_DATASTORE_CACHING_ENABLED_OPTION,
 				),
-				'remote_logging'         => array(
+				'remote_logging'                     => array(
 					'name'               => __( 'Remote Logging', 'woocommerce' ),
 					'description'        => sprintf(
 						/* translators: %1$s: opening link tag, %2$s: closing link tag */
@@ -337,7 +337,7 @@ class FeaturesController {
 						},
 					),
 				),
-				'email_improvements'     => array(
+				'email_improvements'                 => array(
 					'name'            => __( 'Email improvements', 'woocommerce' ),
 					'description'     => __(
 						'Enable modern email design and live preview for transactional emails',
@@ -356,7 +356,7 @@ class FeaturesController {
 					'is_legacy'       => true,
 					'is_experimental' => false,
 				),
-				'blueprint'              => array(
+				'blueprint'                          => array(
 					'name'               => __( 'Blueprint (beta)', 'woocommerce' ),
 					'description'        => __(
 						'Enable blueprint to import and export settings in bulk',
@@ -376,7 +376,7 @@ class FeaturesController {
 					'is_legacy'          => true,
 					'is_experimental'    => false,
 				),
-				'reactify-classic-payments-settings'              => array(
+				'reactify-classic-payments-settings' => array(
 					'name'               => __( 'Payment Settings (beta)', 'woocommerce' ),
 					'description'        => __(
 						'Enable the new payments settings experience',
@@ -396,7 +396,7 @@ class FeaturesController {
 					'is_legacy'          => true,
 					'is_experimental'    => false,
 				),
-				'block_email_editor'     => array(
+				'block_email_editor'                 => array(
 					'name'               => __( 'Block Email Editor (alpha)', 'woocommerce' ),
 					'description'        => __(
 						'Enable the block-based email editor for transactional emails',
